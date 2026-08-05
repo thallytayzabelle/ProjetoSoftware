e86108edfd717633058097a360fc818fa1c78e74
thallytayzabelle <thallytayzabelle@gmail.com>
2026-08-05 10:54:38 -0300

    aula2

---
+++ Aula02-IntroduçãoPOO/IntroducaoPOO/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o nome do Produto: ");
+        String produto1 = sc.nextLine();
+        System.out.print("Digite a quantidade do Produto:");
+        int quantidade = sc.nextInt();
+        System.out.print("Digite o preço do Produto: ");
+        double preco = sc.nextDouble();
+
+        double precoFinal = preco * quantidade;
+
+        System.out.println("Produto:" + produto1
+                + "Quantidade: " + quantidade +
+                "Preco: " + preco +
+                "Preco Final: " + precoFinal
+        );
+
+
+    }
+}
